57f426f915963c0f8151b99482361f3f824866e2
Shahrukh Memon <shahrukh.memon1@intersyslimited.org>
2026-06-13 02:51:24 +0500

    01 Folder done

---
+++ 01_folder/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+      <div id='dad'>
+        <h1 id='child1'>hello</h1>
+      </div>
+      <div id='chacha'>
+        <h1 id='child2'>hello2</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
