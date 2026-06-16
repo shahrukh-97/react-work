dfe2622cea09fc3a22fd046ac58ca0705f4ab0ef
Shahrukh Memon <shahrukh.memon1@intersyslimited.org>
2026-06-16 23:12:39 +0500

    06 TAILWIND DONE

---
+++ 06_tailwind/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='h-screen w-full bg-red-950 text-center' >
+      <h1 className='text-white text-5xl font-bold'>
+        App 
+      </h1>
+    </div>
+  )
+}
+
+export default App
